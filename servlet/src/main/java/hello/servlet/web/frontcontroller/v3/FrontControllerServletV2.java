e2047d569374e980acbd56292404343019ee5731
dolgogae <ohsh0731@gmail.com>
2022-07-25 16:01:08 +0900

    add servlet model

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV2.java
@@ -0,0 +1,55 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV2 extends HttpServlet{
+
+    private final Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV2(){
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+        String requestUrl = request.getRequestURI();
+        
+        // /front-controller/v2/members
+        ControllerV3 controller = controllerMap.get(requestUrl);
+
+        if(controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = new HashMap<>();
+
+        request.getParameterNames().asIterator()
+        .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+
+        ModelView modelView = controller.process(paramMap);
+
+        String viewName = modelView.getViewName();
+        MyView view = new MyView("/WEB-INF/views/"+viewName+"jsp");
+
+        view.render(modelView.getModel(), request, response);
+    }
+}
