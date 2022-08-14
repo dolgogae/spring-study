ce48d7c6d9728e663e78c2b52a70fd244d2caccb
dolgogae <ohsh0731@gmail.com>
2022-08-14 19:26:02 +0900

    http resonse message body

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -10,12 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+// @Controller
+// @ResponseBody
+@RestController
 public class ResponseBodyController {
     
 
@@ -29,7 +32,7 @@ public class ResponseBodyController {
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
-    @ResponseBody
+    // @ResponseBody
     @GetMapping("/response-body-string-v3")
     public String responseBodyV3(){
         return "ok";
@@ -46,7 +49,7 @@ public class ResponseBodyController {
 
 
     @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
+    // @ResponseBody
     @GetMapping("/response-body-json-v2")
     public HelloData responseBodyJsonV2(){
         HelloData helloData = new HelloData();
