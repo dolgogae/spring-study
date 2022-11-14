8043df65b15080f60c66d52d83564eb7749b2937
dolgogae <ohsh0731@gmail.com>
2022-11-14 12:08:39 +0900

    add test

---
+++ jdbc/src/test/java/hello/repository/MemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package hello.repository;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV0;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemberRepositoryTest {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException{
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+    }
+}
