da36587f87bcfc9a4071e728d60e12891db340fb
dolgogae <ohsh0731@gmail.com>
2022-11-15 16:00:42 +0900

    transaction AOP

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -57,17 +57,6 @@ public class MemberServiceV3_1 {
 
     }
 
-    private void release(Connection con){
-        if(con != null){
-            try{
-                con.setAutoCommit(true);        // 커넥션 풀 고려
-                con.close();
-            }catch(Exception e){
-                log.info("error", e);
-            }
-        }
-    }
-
     private void validation(Member toMember){
         if(toMember.getMemberId().equals("ex")){
             throw new IllegalStateException("이체중 예외 발생");
