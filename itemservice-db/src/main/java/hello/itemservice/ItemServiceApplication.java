e0cf1f20f598390ad571ba137d4524711928a52e
dolgogae <ohsh0731@gmail.com>
2022-11-20 21:51:31 +0900

    활용 방안

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -20,7 +20,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(V2Config.class)
 // 수동으로 component scan을 해준다. 안할 경우에 hello하위 모든 것을 scan할 수 있다.
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
