18be78bfdc723818fa3893e3aec02a380a7e76f8
dolgogae <ohsh0731@gmail.com>
2022-11-20 17:07:27 +0900

    mybatis 추가설명

--- itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -5,6 +5,7 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +23,13 @@ public interface ItemMapper {
 
     void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateDto);
 
+    /**
+     * @Select
+     * 다음과 같이 직접 쿼리를 입력해주는 방법도 있다.
+     * 하지만 xml 자체의 장점이 많아서 잘 쓰진 않는다.
+     * 간단한 쿼리를 사용할때 사용.(동적쿼리 불가능)
+     */
+//    @Select("select id, item_name, price, quantity from item where id=#{id}")
     List<Item> findAll(ItemSearchCond itemSearch);
 
     Optional<Item> findById(Long id);
